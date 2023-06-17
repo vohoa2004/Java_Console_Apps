b29374373de09533602ec7eb0d86c99626da219e
Phạm Mạc Kim Tiên <127570843+pmktien3101@users.noreply.github.com>
2023-06-17 20:51:53 +0700

    Update Student.java

--- StudentManager/src/core/Student.java
+++ StudentManager/src/core/Student.java
@@ -67,6 +67,9 @@ public class Student {
     }
 
    
+    void remove() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
  
     
 }
