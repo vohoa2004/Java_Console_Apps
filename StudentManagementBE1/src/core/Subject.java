3f25011952e21a79735ed8103ab326a13b407994
hoavo <vothimaihoa123@gmail.com>
2023-06-20 11:44:11 +0700

    added project

---
+++ StudentManagementBE1/src/core/Subject.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+/**
+ *
+ * @author user
+ */
+public class Subject {
+
+        private String subjectID;
+        private String subjectName;
+        private int credit;
+
+        public Subject(String subjectID, String subjectName, int credit) {
+                this.subjectID = subjectID;
+                this.subjectName = subjectName;
+                this.credit = credit;
+        }
+
+        public String getSubjectID() {
+                return subjectID;
+        }
+
+        public void setSubjectID(String subjectID) {
+                this.subjectID = subjectID;
+        }
+
+        public String getSubjectName() {
+                return subjectName;
+        }
+
+        public void setSubjectName(String subjectName) {
+                this.subjectName = subjectName;
+        }
+
+        public int getCredit() {
+                return credit;
+        }
+
+        public void setCredit(int credit) {
+                this.credit = credit;
+        }
+
+        @Override
+        public String toString() {
+                return "Subject{" + "subjectID=" + subjectID + ", subjectName=" + subjectName + ", credit=" + credit + '}';
+        }
+
+}
